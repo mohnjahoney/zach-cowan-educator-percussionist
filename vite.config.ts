4a25de80dc1ef00bcb6ed6f8a7dc0449faea30a4
John Mahoney <appleseed@Johns-MacBook-Air-2.local>
2026-03-02 00:40:51 -0800

    BrowserRouter to HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/zach-cowan-educator-percussionist/",
+  base: mode === "production" ? "/zach-cowan-educator-percussionist/" : "/",
   server: {
     host: "::",
     port: 8080,
